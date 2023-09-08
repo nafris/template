b8b2199d1c0b275c00c000e263e02b1e0c758e36
Nauris Vasilevskis <navris@inbox.lv>
2023-09-08 10:17:52 +0100

    argument feature added

--- template.c
+++ template.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(){
+int main(int argc, char *argv[]){
 	printf("Hello world!\n");
+	printf("You have passed %d arguments\n", argc);
+	for (int i=0; i<argc; i++){
+		printf("%s \n",argv[i]);
+	}
 	return 0;
 }
